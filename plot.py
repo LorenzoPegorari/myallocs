549371a663973fda05d24fc525739f634ae23abb
LorenzoPegorari <lorenzo.pegorari2002@gmail.com>
2026-05-09 22:15:28 +0200

    test: improve test and plot
    Improve "test.c" test for data collecting, and improve the "plot.py"
    script to plot the collected data.
    
    Signed-off-by: LorenzoPegorari <lorenzo.pegorari2002@gmail.com>

--- plot.py
+++ plot.py
@@ -1,24 +1,38 @@
 import matplotlib.pyplot as plt
 
-cycle = []
+cycle1 = []
 fragmentation = []
+cycle2 = []
+allocated = []
 
-f = open('data.txt','r')
-for row in f:
+f1 = open('data1.txt','r')
+for row in f1:
     row = row.split(',')
-    cycle.append(int(row[0]))
+    cycle1.append(int(row[0]))
     fragmentation.append(float(row[1]))
+f2 = open('data2.txt','r')
+for row in f2:
+    row = row.split(',')
+    cycle2.append(int(row[0]))
+    allocated.append(int(row[1]))
 
-plt.plot(cycle, fragmentation, color = 'r', label = 'File Data')
+fig, ax1 = plt.subplots()
 
-plt.xlabel('Number of cycles', fontsize = 12)
-plt.ylabel('Fragmentation percentage', fontsize = 12)
+ax1.set_xlabel('Number of cycles', fontsize=12)
+ax1.set_ylabel('Fragmentation percentage', color='r', fontsize=12)
+ax1.set_ylim(0, 100)
+ax1.plot(cycle1, fragmentation, color='r', label='mymalloc()')
+ax1.set_title('Memory fragmentation', fontsize=20)
+ax1.grid(color='r', linestyle=':', linewidth=1)
+ax1.tick_params(axis='y', labelcolor='r')
+ax1.legend(loc='upper left')
 
-plt.xlim(0)
-plt.ylim(0, 100)
+ax2 = ax1.twinx()
 
-plt.grid(color='r', linestyle=':', linewidth=1)
+ax2.set_ylabel('Allocated bytes', color='b', fontsize=12)
+ax2.plot(cycle2, allocated, color='b', label='mymalloc()')
+ax2.tick_params(axis='y', labelcolor='b')
+ax2.legend(loc='upper right')
 
-plt.title('Memory fragmentation', fontsize = 20)
-plt.legend()
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.show()
--- test.c
+++ test.c
@@ -14,14 +14,33 @@
 
 
 void rand_unique_vals_in_range(int *array, int n, int m);
-void run_memory_data_test(const char *data_filename);
+void run_memory_data_test(FILE *file1, FILE *file2);
 
 
 int main(void) {
+    FILE  *file1;
+    FILE  *file2;
+
     // Seed with current time.
     srand(time(NULL));
 
-    run_memory_data_test("data.txt");
+    // Open files
+    file1 = fopen("data1.txt", "w");
+    if (file1 == NULL) {
+        perror("fopen() error");
+        exit(EXIT_FAILURE);
+    }
+    file2 = fopen("data2.txt", "w");
+    if (file2 == NULL) {
+        perror("fopen() error");
+        exit(EXIT_FAILURE);
+    }
+
+    run_memory_data_test(file1, file2);
+
+    // Close files.
+    fclose(file1);
+    fclose(file2);
 
     exit(EXIT_SUCCESS);
 }
@@ -44,29 +63,37 @@ void rand_unique_vals_in_range(int *array, int n, int m) {
 
 
 /**
- * TODO
+ * Very simple (and dumb) function that does the following:
+ *   - allocate ALLOC_MAX blocks of memory, where each of them has a size
+ *     between ALLOC_BLOCK_MIN and ALLOC_BLOCK_MAX.
+ *     Then, take REALLOC_MAX of these blocks of memory randomly chosen, free
+ *     them and reallocate them again with a random size between
+ *     ALLOC_BLOCK_MIN and ALLOC_BLOCK_MAX. Do this REALLOC_CYCLES times.
+ *     Finally, free all allocated memory blocks.
+ *     Print inside `file1` comma-separated data, where each line contains:
+ *         "<current-cycle>,<memory-fragmentation-percentage>"
+ *     <current-cycle> = 0 -------------------> first allocations
+ *     <current-cycle> = [1..REALLOC_CYCLES] -> frees and re-allocs
+ *     <current-cycle> = REALLOC_CYCLES + 1 --> final frees
+ *   - print to stdout the <throughput>, calculated as:
+ *         <number-of-allocs-and-frees>/<time-unit>
  */
-void run_memory_data_test(const char *data_filename) {
-    void  *allocs[ALLOC_MAX];
-    int    idx_to_realloc[REALLOC_MAX];
-    size_t tot_allocated;
-    size_t tot_used;
-    FILE  *file;
-
-    // Open data file
-    file = fopen(data_filename, "w");
-    if (file == NULL) {
-        perror("fopen() error");
-        exit(EXIT_FAILURE);
-    }
+void run_memory_data_test(FILE *file1, FILE *file2) {
+    void   *allocs[ALLOC_MAX];
+    int     idx_to_realloc[REALLOC_MAX];
+    size_t  tot_allocated;
+    size_t  tot_used;
+    clock_t temp;
+    clock_t tot_time = 0;
 
     // First allocate ALLOC_MAX blocks of memory, keeping ptrs inside `allocs`.
     for (int i = 0; i < ALLOC_MAX; ++i) {
         int size = (rand() % (ALLOC_BLOCK_MAX - ALLOC_BLOCK_MIN)) + ALLOC_BLOCK_MIN;
+        temp = clock();
         allocs[i] = mymalloc(size);
+        tot_time += clock() - temp;
         if (allocs[i] == NULL) {
             perror("mymalloc() error");
-            fclose(file);
             exit(EXIT_FAILURE);
         }
     }
@@ -74,8 +101,9 @@ void run_memory_data_test(const char *data_filename) {
     // Print initial cycle situation.
     tot_allocated = get_total_allocated();
     tot_used = get_total_used();
-    fprintf(file, "0,%.2f\n",
+    fprintf(file1, "0,%.2f\n",
             100 - (((double)tot_used) / ((double)tot_allocated) * 100.0));
+    fprintf(file2, "0,%zu\n", tot_allocated);
 
     // Reallocate REALLOC_CYCLES times.
     for (int j = 0; j < REALLOC_CYCLES; ++j) {
@@ -85,14 +113,17 @@ void run_memory_data_test(const char *data_filename) {
             int size;
 
             // Free
+            temp = clock();
             myfree(allocs[idx_to_realloc[i]]);
+            tot_time += clock() - temp;
 
             // Allocate again.
             size = (rand() % (ALLOC_BLOCK_MAX - ALLOC_BLOCK_MIN)) + ALLOC_BLOCK_MIN;
+            temp = clock();
             allocs[idx_to_realloc[i]] = mymalloc(size);
+            tot_time += clock() - temp;
             if (allocs[i] == NULL) {
                 perror("mymalloc() error");
-                fclose(file);
                 exit(EXIT_FAILURE);
             }
         }
@@ -100,20 +131,25 @@ void run_memory_data_test(const char *data_filename) {
         // Print current cycle situation.
         tot_allocated = get_total_allocated();
         tot_used = get_total_used();
-        fprintf(file, "%d,%.2f\n", j + 1,
+        fprintf(file1, "%d,%.2f\n", j + 1,
                 100 - (((double)tot_used) / ((double)tot_allocated) * 100.0));
+        fprintf(file2, "%d,%zu\n", j + 1, tot_allocated);
     }
 
     // Free all blocks of memory.
-    for (int i = 0; i < ALLOC_MAX; ++i)
+    for (int i = 0; i < ALLOC_MAX; ++i){
+        temp = clock();
         myfree(allocs[i]);
+        tot_time += clock() - temp;
+    }
 
     // Print final cycle situation.
     tot_allocated = get_total_allocated();
     tot_used = get_total_used();
-    fprintf(file, "%d,%.2f\n", REALLOC_CYCLES + 1,
+    fprintf(file1, "%d,%.2f\n", REALLOC_CYCLES + 1,
             100 - (((double)tot_used) / ((double)tot_allocated) * 100.0));
+    fprintf(file2, "%d,%zu\n", REALLOC_CYCLES + 1, tot_allocated);
 
-    // Close data file.
-    fclose(file);
+    printf("Throughput: %.3f mymallocs and frees per clock_t\n",
+           (double)(ALLOC_MAX * 2 + REALLOC_CYCLES * REALLOC_MAX * 2) / (double)tot_time);
 }
